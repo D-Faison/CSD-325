ed4c3eb4afe55ed574de662fc55ceaa34e57ab8e
DFaison <dfaison2017@gmail.com>
2025-02-16 16:27:30 -0500

    Finished

--- module-7/city_functions.py
+++ module-7/city_functions.py
@@ -1,16 +1,31 @@
-#DeJanae Faison DATE Assignment 7.2
+#DeJanae Faison 2/16/24 Assignment 7.2
 #Test Cases;
 
-def city_country(city,country,population):
+def city_country(city,country,population = None, language = None):
     #Format the city name
-    formatted_City = f"{city},{country}-population:{population}"
-    return(formatted_City)
+    formatted_City = f"{city},{country}"
+
+    #Optional Parameters
+    if population and language:
+        #The Variable has all 4 parameters
+        return(f"{formatted_City}-population:{population},{language}")
+    #Variable has the city, country and language, NOT Population
+    elif language:
+        return(f"{formatted_City}-{language}")
+    #Variable has the city, country and population but NOT language
+    elif population:
+        return(f"{formatted_City}-population:{population}")
+    #Has city and country but NOT population and language
+    else:
+        return(formatted_City)
+    
+
 
 #Several City Variables
-city1 = city_country("Tokyo","Japan",4000)
+city1 = city_country("Tokyo","Japan")
 city2 = city_country("Beijing","China",8000)
-city3 = city_country("Jersey City","USA",2000)
-
+city3 = city_country("Jersey City","USA",2000,"English")
+#Display City Variables
 print()
 print(city1)
 print()
--- module-7/test_cities.py
+++ module-7/test_cities.py
@@ -1,4 +1,4 @@
-#DeJanae Faison
+#DeJanae Faison 2/16/24 Assignment TestCases
 
 #import file function
 from city_functions import city_country
@@ -11,9 +11,9 @@ class testingMethods(unittest.TestCase):
     def test_city_country(self):
       #does the value from the function == the desireed answer 
       #when inputting the city and country does it = City,Country
-      self.assertEqual(city_country("Tokyo","Japan",4000),"Tokyo,Japan")
-      self.assertEqual(city_country("Beijing","China",8000),"Beijing,China")
-      self.assertEqual(city_country("Jersey City","USA",2000),"Jersey City,USA")
+      self.assertEqual(city_country("Tokyo","Japan"),"Tokyo,Japan")
+      self.assertEqual(city_country("Beijing","China"),"Beijing,China")
+      self.assertEqual(city_country("Jersey City","USA"),"Jersey City,USA")
 
 #run the script
 if __name__ == '__main__':
